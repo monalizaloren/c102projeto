666ed1db41f1a817331cfd20fa87f116f0a13a0e
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-08-18 17:42:45 -0300

    Update Move_Files.py

--- Move_Files.py
+++ Move_Files.py
@@ -1,20 +1,17 @@
 import os
 import shutil
 
-# .exe , .msi,  .gif, .png .jpg, .jpeg, .csv, .pdf , .xls , .xlsx , .ppt , .pptx
-
-from_dir = "C:/Users/jaime/Downloads"
-to_dir = "C:/Byjus/imagensbaixadas"
+from_dir = ???
+to_dir = ???
 
 list_of_files = os.listdir(from_dir)
 print(list_of_files)
 
-# Mova todos os arquivos de imagem da pasta Downloads para outra pasta
 for file_name in list_of_files:
 
     name, extension = os.path.splitext(file_name)
-    #print(name)
-    #print(extension)
+    #printe a variável name e extension
+ 
 
     if extension == '':
         continue
@@ -23,17 +20,12 @@ for file_name in list_of_files:
         path1 = from_dir + '/' + file_name                         # Exemplo path1 : Downloads/nomeImagem1.jpg        
         path2 = to_dir + '/' + "Arquivos_Documentos"                   # Exemplo path2 : D:/Meus Arquivos/Arquivos_Imagem      
         path3 = to_dir + '/' + "Arquivos_Documentos" + '/' + file_name # Exemplo path3 : D:/Meus Arquivos/Arquivos_Imagem/nomeImagem1.jpg
-        #print("path1 " , path1)
-        #print("path3 ", path3)
-
-        # Verifique se o caminho da pasta/diretório existe antes de mover
-        # Caso contrário, crie uma NOVA pasta/diretório, e então mova
-        
+       
         if os.path.exists(path2):
           print("Movendo " + file_name + ".....")
 
           # Mover de path1 ---> path3
-          shutil.move(path1, path3)
+          shutil.move(??, ???)
 
         else:
           os.makedirs(path2)
